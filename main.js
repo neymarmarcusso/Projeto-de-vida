8e77c56366e846c0215191ac82685f7a359e793d
neymarmarcusso <162704187+neymarmarcusso@users.noreply.github.com>
2025-05-20 09:53:54 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -35,21 +35,25 @@ function calculaTempo(tempoObjetivo){
     horas %= 24;
 
   if (tempoFinal > 0){
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+    return [dias,horas,minutos,segundos];
   } else {
-    return "Prazo Finalizado";
-}
+    return [0,0,0,0];
+  }
 }
 
 function atualizaCronometro(){
-    for (let i = 0; i < contadores.length; i++) {
-        contadores[i].textContent = calculaTempo(tempos[i]);
+    document.getElementById("dias0").textContent = calculaTempo(tempos[1])[0];
+    document.getElementById("horas0").textContent = calculaTempo(tempos[1])[1];
+    document.getElementById("min0").textContent = calculaTempo(tempos[1])[2];
+    document.getElementById("seg0").textContent = calculaTempo(tempos[1])[3];
+    for (let i = 0; i < contadores.length; i++){
+      //  contadores[i].textContent = calculaTempo(tempos[i]);
     }
-    }
+}
     
     function comecaCronometro(){
     atualizaCronometro();
     setInterval(atualizaCronometro,100);
     }
 
-    comecaCronometro();
+    // comecaCronometro();
